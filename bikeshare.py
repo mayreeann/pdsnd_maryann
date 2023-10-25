15628779e6abbd673439bd31d3fc0e32c099e187
Amaefula Maryann <amaannmary006@gmail.com>
2023-10-25 11:31:06 +0100

    refactor : display max columns

--- bikeshare.py
+++ bikeshare.py
@@ -160,6 +160,9 @@ def trip_duration_stats(df):
 def user_stats(df):
     """Displays statistics on bikeshare users."""
 
+    # Set the display option to show all columns
+    pd.set_option("display.max_columns", 200)
+
     print('\nCalculating User Stats...\n')
     start_time = time.time()
 
